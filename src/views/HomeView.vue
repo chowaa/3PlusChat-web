819daff5d8993f9bd4dc8d6d970bc2cb966399af
chowaa <17671841751@163.com>
2023-04-26 16:13:48 +0800

    小测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="home">
-    <el-button>111</el-button>
-    home
+    <el-input v-model="inputVal"></el-input>
+    <el-button @click="sendData">111</el-button>
+    {{ getVal }}
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const inputVal = ref('')
+const getVal = ref('')
+const ws = new WebSocket('ws://localhost:8880/webscoket')
+ws.onopen = () => {
+  console.log('WebSocket连接已打开');
+};
+ws.onmessage = (event) => {
+  getVal.value = event.data
+  console.log('收到WebSocket消息:', event.data);
+};
+
+const sendData = ()=> {
+  ws.send(inputVal.value)
+}
 </script>
