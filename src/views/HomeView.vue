6faaa5d08d729ec1f8ee699c09a68cc415b8187b
chowaa <17671841751@163.com>
2023-04-27 09:50:19 +0800

    设置连接或未连接提示

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <el-button @click="open">建立连接</el-button>
-    <el-input v-model="inputVal"></el-input>
+    <el-input v-model="inputValRef"></el-input>
     <el-button @click="sendData">111</el-button>
-    {{ getVal }}
+    {{ getValRef }}
     <el-button @click="close">关闭连接</el-button>
   </div>
 </template>
@@ -11,22 +11,36 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import WS from '@/http/ws';
+import { ElMessage } from 'element-plus';
 let ws :WS
 
-const inputVal = ref('123')
-const getVal = ref('')
+const inputValRef = ref('123')
+const getValRef = ref('')
+const createdWsRef = ref(false)
 
 const open = ()=> {
   ws = new WS({url:'ws://localhost:8880/webscoket', pingMsg:'{"type":"0000","data":"heart check"}'})
+  createdWsRef.value = true;
+  ElMessage.success('连上了？狠狠的向我发送信息吧')
 }
 
 const sendData = ()=> {
-  ws.send(inputVal.value)
-  ws.onmessage = evt => {
-    console.log(evt.data);
+  if (createdWsRef.value) {
+    ws.send(inputValRef.value)
+    ws.onmessage = event => {
+      getValRef.value = event.data
+      console.log(event.data);
+    }
+  } else {
+    ElMessage.error('先连啊！')
   }
 }
 const close = ()=> {
-  ws.close()
+  if (createdWsRef.value) {
+    ws.close()
+    ElMessage.success('拜拜嘞')
+  } else {
+    ElMessage.error('嗯？没连你都想关！big胆！')
+  }
 }
 </script>
